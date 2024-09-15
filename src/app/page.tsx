03442d005f3830790754bde2b5695da0814b2c36
tibo-koninckx <r0929614@student.thomasmore.be>
2024-09-15 10:29:27 +0200

    change flexdirection to column

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 import {Flex, Typography} from "antd";
+import {PDFViewer} from "@react-pdf/renderer";
+import {GeneratePdf} from "@/components/generatePdf";
 
 export default function Home() {
   return <>
     <Flex align="center" vertical>
       <Typography.Title>Generate pdf</Typography.Title>
+      <PDFViewer>
+        <GeneratePdf />
+      </PDFViewer>
     </Flex>
   </>
 }
