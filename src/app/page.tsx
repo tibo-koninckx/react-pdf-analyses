12f7e95c9db7b8b596ed56a95a791883d1d3fa39
tibo-koninckx <r0929614@student.thomasmore.be>
2024-09-15 10:44:33 +0200

    rename pdf file and formate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import {PDFDownloadLink, PDFViewer} from "@react-pdf/renderer";
 import {GeneratePdf} from "@/components/generatePdf";
 
 export default function Home() {
-  return <>
-    <Flex align="center" vertical>
-      <Typography.Title>Generate pdf</Typography.Title>
-      <PDFViewer width="20%" height={500} showToolbar={true}>
-        <GeneratePdf />
-      </PDFViewer>
-        <PDFDownloadLink document={<GeneratePdf />} fileName="Anlyses data">
-          {({blob, url, loading, error}) => (loading ? "Loading document..." : "Download now!")}</PDFDownloadLink>
-    </Flex>
-  </>
+    return <>
+        <Flex align="center" vertical>
+            <Typography.Title>Generate pdf</Typography.Title>
+            <PDFViewer width="20%" height={500} showToolbar={true}>
+                <GeneratePdf/>
+            </PDFViewer>
+            <PDFDownloadLink document={<GeneratePdf/>} fileName="Analyses data">
+                {({blob, url, loading, error}) => (loading ? "Loading document..." : "Download now!")}</PDFDownloadLink>
+        </Flex>
+    </>
 }
